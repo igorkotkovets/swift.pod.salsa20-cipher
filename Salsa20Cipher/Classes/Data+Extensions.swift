c0f5d059704041d48b9bee5ed71889b83e37ba50
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2017-12-18 19:02:52 +0300

    Update OutputStream protocol implementation

--- Salsa20Cipher/Classes/Data+Extensions.swift
+++ Salsa20Cipher/Classes/Data+Extensions.swift
@@ -8,57 +8,6 @@
 import Foundation
 
 public extension Data {
-
-    public init?(hex string: String) {
-        func decodeNibble(character: UInt16) -> UInt8? {
-            switch character {
-            case 0x30 ... 0x39: // 0..9
-                return UInt8(character - 0x30)
-            case 0x41 ... 0x46: // A..F
-                return UInt8(character - 0x41 + 10)
-            case 0x61 ... 0x66: // a..f
-                return UInt8(character - 0x61 + 10)
-            default:
-                return nil
-            }
-        }
-
-        self.init(capacity: string.utf16.count/2)
-        var even = true
-        var byte: UInt8 = 0
-
-        for c in string.utf16 {
-            guard let val = decodeNibble(character: c) else {
-                return nil
-            }
-
-            if even {
-                byte = val << 4
-            } else {
-                byte += val
-                append(byte)
-            }
-            even = !even
-        }
-
-        guard even else {
-            return nil
-        }
-    }
-
-    var hexString: String {
-        var result = ""
-
-        var bytes = [UInt8](repeating: 0, count: count)
-        copyBytes(to: &bytes, count: count)
-
-        for byte in bytes {
-            result += String(format: "%02x", UInt(byte))
-        }
-
-        return result
-    }
-
     func salsa20Encrypted(withKey: String, initializationVector: String) -> Data? {
         guard let keyData = withKey.data(using: .utf8),
             let ivData = initializationVector.data(using: .utf8) else {
