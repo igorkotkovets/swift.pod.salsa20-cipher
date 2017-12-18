749565f91b1df1bf95420117cfbcc0db8f38af86
Igor Kotkovets <igorkotkovets@users.noreply.github.com>
2017-12-18 19:23:44 +0300

    Optimise extensions

--- Salsa20Cipher/Classes/Salsa20CipherStream.swift
+++ Salsa20Cipher/Classes/Salsa20CipherStream.swift
@@ -74,3 +74,40 @@ public class Salsa20CipherStream: InputStream {
         return true
     }
 }
+
+public extension String {
+    func salsa20Encrypted(withKey key: String, initializationVector vector: String) -> Data? {
+        guard let strData = self.data(using: .utf8) else {
+            return nil
+        }
+
+        return strData.salsa20Encrypted(withKey: key, initializationVector: vector)
+    }
+}
+
+public extension Data {
+    func salsa20Encrypted(withKey: String, initializationVector: String) -> Data? {
+        guard let keyData = withKey.data(using: .utf8),
+            let ivData = initializationVector.data(using: .utf8) else {
+                return nil
+        }
+
+        let dataStream = DataInputStream(withData: self)
+        do {
+            let salsa20Stream = try Salsa20CipherStream(withStream: dataStream, key: keyData, iv: ivData)
+            var resultData = Data(count: self.count)
+            var readLength: Int?
+            resultData.withUnsafeMutableBytes { (bytes: UnsafeMutablePointer<UInt8>) -> Void in
+                readLength = salsa20Stream?.read(bytes, maxLength: self.count)
+            }
+
+            guard readLength == self.count else {
+                return nil
+            }
+
+            return resultData
+        } catch {
+            return nil
+        }
+    }
+}
